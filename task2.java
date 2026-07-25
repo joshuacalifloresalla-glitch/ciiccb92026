0150d8c8fe4724773b4f6baa6dc2c5961f39478b
joshuacalifloresalla-glitch <joshuacalifloresalla@gmail.com>
2026-07-25 10:48:32 +0800

    Create task2.java

---
+++ task2.java
@@ -0,0 +1,23 @@
+public class Task2 {
+    public static void main(String[] args) {
+
+        char letter = 'H';
+        short three = 3;
+        byte one = 1;
+        int ten = 10;
+        char space = ' ';
+        char w = 'w';
+        byte zero = 0;
+        char r = 'r';
+        char l = 'l';
+        char d = 'd';
+        float two = 2.0f;
+        boolean truth = true;
+
+        String output = "" + letter + three + one + one + zero + space
+                + w + zero + r + l + d + space
+                + two + space + truth;
+
+        System.out.println(output);
+    }
+}
